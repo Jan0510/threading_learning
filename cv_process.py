6b0aa71e8a5670d09474167b81ccb44a30b5d886
Jan0510 <814862021@qq.com>
2020-11-21 17:38:02 +0800

    1

---
+++ cv_process.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Author  :   {Jan__}
+# @Time    :   2020/11/19 10:46
+
+class cv_process:
+    def cv_connect(self):
+        
+
+if __name__ == '__main__':
+    pass
